877be6c4cecac261b45a1b20fd983116eb87ad89
JoshSald <42002468+JoshSald@users.noreply.github.com>
2025-10-02 10:57:58 +0200

    Checkout Functionality, Add More Weird Styling

--- scripts/domElements.js
+++ scripts/domElements.js
@@ -1,4 +1,5 @@
 import { addToCart, getCart } from "./localStorage.js";
+const checkout = document.querySelector("#checkout");
 
 export function createProductCard(product, onAddToCart) {
   const card = document.createElement("div");
@@ -13,17 +14,21 @@ export function createProductCard(product, onAddToCart) {
   title.textContent = product.title;
   title.className = "font-semibold text-lg mb-2 line-clamp-2";
 
+  const description = document.createElement("p");
+  description.textContent = product.description;
+  description.className = "mb-4 line-clamp-2";
+
   const price = document.createElement("p");
   price.textContent = `$${product.price}`;
-  price.className = "text-green-600 font-bold mb-4";
+  price.className = "text-green-600 font-bold mb-2";
 
   const button = document.createElement("button");
   button.textContent = "Add to Cart";
   button.className =
-    "mt-auto bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 rounded";
+    "mt-auto bg-emerald-900 hover:bg-green-500 text-white px-4 py-2 rounded cursor-pointer";
   button.addEventListener("click", () => onAddToCart(product));
 
-  card.append(img, title, price, button);
+  card.append(img, title, price, description, button);
   return card;
 }
 
@@ -37,14 +42,8 @@ export function renderProducts(products, container, onAddToCart) {
 
 export function renderCart(container, onRemove) {
   const cart = getCart();
-  const checkout = document.querySelector("#checkout");
-  if (cart.length > 0) {
-    checkout.classList.remove("hidden");
-  } else {
-    checkout.classList.add("hidden");
-  }
-
   container.innerHTML = "";
+  cartHelper(cart.length);
   cart.forEach((item) => {
     const li = document.createElement("li");
     li.className =
@@ -56,13 +55,24 @@ export function renderCart(container, onRemove) {
     const button = document.createElement("button");
     button.textContent = "Remove";
     button.className =
-      "ml-4 bg-red-500 hover:bg-red-400 text-white px-2 py-1 rounded text-sm";
+      "ml-4 bg-red-500 hover:bg-red-400 text-white px-2 py-1 rounded text-sm cursor-pointer";
     button.addEventListener("click", () => onRemove(item.id));
 
     li.append(text, button);
     container.appendChild(li);
   });
-  checkout.addEventListener("click", () => {
-    alert("Sorry. It was just too good to be true.");
-  });
 }
+
+function cartHelper(items) {
+  const cart = document.querySelector("#cart");
+  if (items > 0) {
+    checkout.classList.remove("hidden");
+    cart.classList.remove("hidden");
+  } else {
+    checkout.classList.add("hidden");
+    cart.classList.add("hidden");
+  }
+}
+checkout.addEventListener("click", () => {
+  alert("Sorry. It was just too good to be true.");
+});
